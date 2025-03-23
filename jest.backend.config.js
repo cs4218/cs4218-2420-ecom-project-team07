a41af6b8ff803a6f8e8a686a6cd8ea5d532fc6e3
Huang Zhenxu <zhenxu@u.nus.edu>
2025-03-23 09:31:21 +0800

    Fix Typo and Modify jest.backend.config.js to test all backend test cases.

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -7,27 +7,21 @@ export default {
 
   // which test to run
   testMatch: [
-    // "<rootDir>/config/*.test.js",
-    // "<rootDir>/controllers/*.test.js",
-    // "<rootDir>/helpers/*.test.js",
-    // "<rootDir>/middlewares/*.test.js",
-    // "<rootDir>/models/*.test.js",
-    "<rootDir>/controllers/authController.test.js",
-    "<rootDir>/controllers/categoryController.test.js",
-    "<rootDir>/controllers/authController.integration.test.js",
-    "<rootDir>/controllers/categoryController.integration.test.js",
+    "<rootDir>/config/*.test.js",
+    "<rootDir>/controllers/*.test.js",
+    "<rootDir>/helpers/*.test.js",
+    "<rootDir>/middlewares/*.test.js",
+    "<rootDir>/models/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    // "config/**",
-    // "controllers/**",
-    // "helpers/**",
-    // "middlewares/**",
-    // "models/**",
-    "controllers/authController.js",
-    "controllers/categoryController.js",
+    "config/**",
+    "controllers/**",
+    "helpers/**",
+    "middlewares/**",
+    "models/**",
   ],
   coverageThreshold: {
     global: {

8cae7b6fb37b26e194943d8210df11fa1a3b0406
Cloud <44526554+Cloud7050@users.noreply.github.com>
2025-03-24 09:28:17 +0800

    fix: Contact page tests & fixes

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Layout from "./../components/Layout";
 import { BiMailSend, BiPhoneCall, BiSupport } from "react-icons/bi";
+import Layout from "./../components/Layout";
 
 const Contact = () => {
   return (
-    <Layout title={"Contact us"}>
-      <div className="row contactus ">
+    <Layout title={"Contact Us"}>
+      <div className="row contactus">
         <div className="col-md-6 ">
           <img
             src="/images/contactus.jpeg"
@@ -16,17 +16,16 @@ const Contact = () => {
         <div className="col-md-4">
           <h1 className="bg-dark p-2 text-white text-center">CONTACT US</h1>
           <p className="text-justify mt-2">
-            For any query or info about product, feel free to call anytime. We are
-            available 24X7.  
+            For any queries, feel free to contact us via the methods below. We are available 24/7.
           </p>
           <p className="mt-3" data-testid="MailSend">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : contact@virtualvault.com
           </p>
           <p className="mt-3" data-testid="PhoneCall">
-            <BiPhoneCall /> : 012-3456789
+            <BiPhoneCall /> : +65 6246 7050
           </p>
           <p className="mt-3" data-testid="Support">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport /> : 1800-6565-6565 (toll-free)
           </p>
         </div>
       </div>
